488188891fe5fb3315057fa8c88d3da9fcbf5914
Anuja <2025it0319@svce.ac.in>
2026-03-17 14:38:10 +0000

    byteSize

---
+++ size.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int integer_type;
+	char character_type;
+	float float_type;
+	double double_type;
+	cout<<"size of int is:"<<sizeof(integer_type)<<endl;
+	cout<<"size of character is:"<<sizeof(character_type)<<endl;
+	cout<<"size of float is:"<<sizeof(float_type)<<endl;
+	cout<<"size of double is:"<<sizeof(double_type)<<endl;
+	return 0;
+}
